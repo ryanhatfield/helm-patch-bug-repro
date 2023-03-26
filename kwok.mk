77b43e506c33d1ee5d07a70c93010a4b4d9d25a5
Ryan Hatfield <rhatfield@bushelpowered.com>
2023-03-26 01:06:40 -0500

    Update kwok makefile

--- kwok.mk
+++ kwok.mk
@@ -1,13 +1,14 @@
 # Helper makefile for using kwok as part of testing
-#	https://kwok.sigs.k8s.io/
-#	Author: Ryan Hatfield
+# https://kwok.sigs.k8s.io/
+# Author: Ryan Hatfield
+# https://gist.github.com/ryanhatfield/1ec81e468abf8cd0803b4d578adb1de4
 
 CLUSTER_NAME:=$(shell basename $$PWD)
 export KUBECONFIG=$(CLUSTER_NAME).kubeconfig
 KWOKCTL:=kwokctl --name=$(CLUSTER_NAME)
 
 isRunning=$(patsubst $(1),true,$(filter $(shell $(KWOKCTL) get clusters),$(1)))
-isNotRunning=$(if $(call isRunning, $1),,true)
+isNotRunning=$(if $(call isRunning, $(1)),,true)
 fileExists=$(patsubst $(1),true,$(wildcard $(1)))
 
 kwok-create: ## Create cluster if not running
